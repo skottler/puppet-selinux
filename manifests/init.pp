b9f3a7d92ab2ecca77935539e2c319e3203052a0
Sam Kottler <shk@redhat.com>
2013-01-10 11:42:13 -0500

    Only attempt to do SElinux-related validation on RHEL derivatives

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,11 +2,15 @@ class selinux(
   $mode,
   $application,
 ) {
-  if ($selinux_current_mode != $mode) {
-    if ($selinux_config_mode == $mode) {
-      notice("The SElinux mode is set properly (to $selinux_config_mode) in configuration, but not currently in $selinux_current_mode, which will cause problems for $application. Restart the machine to correct this.")
-    } else {
-      notice("The SElinux mode is set to $selinux_config_mode, but $application requires it to be $mode")
+  case $operatingsystem {
+    'CentOS', 'Fedora', 'RedHat': {
+      if ($selinux_current_mode != $mode) {
+        if ($selinux_config_mode == $mode) {
+          notice("The SElinux mode is set properly (to $selinux_config_mode) in configuration, but not currently in $selinux_current_mode, which will cause problems for $application. Restart the machine to correct this.")
+        } else {
+          notice("The SElinux mode is set to $selinux_config_mode, but $application requires it to be $mode")
+        }
+      }
     }
   }
 }
